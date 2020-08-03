57752dfc77a024ec97ba6c76e2c2be7362384e44
Narender Kumar <narender78966@gmail.com>
2020-08-04 03:01:42 +0530

    man jumping over obstacles in urban area

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float gravityMod = 1;
+    public float jumpForce = 10;
+    public ParticleSystem explosionEffect;
+    public ParticleSystem dirtEffect;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    public AudioSource playerSound;
+    public bool touchGround = true;
+    public bool gameOver;
+    private Animator playerAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+        playerAnim = GetComponent<Animator>();
+        playerSound = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && touchGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            touchGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtEffect.Stop();
+            playerSound.PlayOneShot(jumpSound, 1.0f);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            touchGround = true;
+            dirtEffect.Play();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionEffect.Play();
+            dirtEffect.Stop();
+            playerSound.PlayOneShot(crashSound, 1.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefab;
+    private Vector3 spawnLoc = new Vector3(25, 0, 0);
+    private float delayTime = 2;
+    private float repeatTime = 2;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("spawnObstacle", delayTime, repeatTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void spawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            int obstacleIndex = Random.Range(0, obstaclePrefab.Length);
+            Instantiate(obstaclePrefab[obstacleIndex], spawnLoc, obstaclePrefab[obstacleIndex].transform.rotation);
+        }
+    }
+}
